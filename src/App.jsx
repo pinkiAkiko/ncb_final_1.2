3b47946f2facfcf437548b51227daf6202bca664
pinkiAkiko <pinki.akiko@gmail.com>
2026-04-24 10:47:03 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.scss";
+import AppRoutes from "./routes/AppRoutes";
+import ScrollToTop from "./components/ScrollToTop";
+
+
+function App() {
+  return (
+    <>
+      <ScrollToTop />
+      <AppRoutes />
+    </>
+  );
+}
+
+export default App;
